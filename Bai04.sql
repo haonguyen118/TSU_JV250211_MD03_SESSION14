bd6b6613263658d5cfdf90ed3c48de124d866838
haonguyen118 <mailatoi88@gmail.com>
2025-07-15 07:58:34 +0900

    session14

---
+++ Bai04.sql
@@ -0,0 +1,44 @@
+-- Tạo và sử dụng database
+CREATE DATABASE session14;
+USE session14;
+
+-- Tạo bảng Orders
+CREATE TABLE Orders (
+    OrderID INT AUTO_INCREMENT PRIMARY KEY,              
+    CustomerName VARCHAR(100) NOT NULL,                 
+    ProductName VARCHAR(100) NOT NULL,                  
+    Quantity INT NOT NULL CHECK (Quantity > 0),         
+    Price DECIMAL(10, 2) NOT NULL CHECK (Price > 0),    
+    OrderDate DATE NOT NULL                             
+);
+
+-- Thêm giá trị vào bảng Orders
+INSERT INTO Orders (CustomerName, ProductName, Quantity, Price, OrderDate)
+VALUES
+    ('Nguyen Van A', 'Laptop', 1, 15000000, '2025-01-01'),
+    ('Tran Thi B', 'Smartphone', 2, 8000000, '2025-01-01'),
+    ('Nguyen Van A', 'Headphones', 3, 2000000, '2025-01-03'),
+    ('Le Van C', 'Laptop', 1, 15000000, '2025-01-01'),
+    ('Nguyen Van A', 'Smartphone', 1, 8000000, '2025-01-05'),
+    ('Tran Thi B', 'Headphones', 1, 2000000, '2025-01-05'),
+    ('Le Van C', 'Smartphone', 3, 8000000, '2025-01-07'),
+    ('Tran Thi B', 'Laptop', 1, 15000000, '2025-01-03');
+
+#2.
+select CustomerName, sum(Quantity) as TotalQuantity
+from orders
+group by CustomerName;
+#3
+select ProductName, max(Price) as MaxPrice
+from orders
+group by ProductName;
+
+#4
+select OrderDate, count(OrderId) as TotalOrders
+from Orders
+group by OrderDate;
+
+#5
+select CustomerName, min(Price) as MinPrice
+from Orders
+group by CustomerName;
---
+++ Bai05.sql
@@ -0,0 +1,33 @@
+USE session14;
+-- Tạo bảng Products
+CREATE TABLE Products (
+    ProductID INT AUTO_INCREMENT PRIMARY KEY,  -- Khóa chính tự động tăng
+    ProductName VARCHAR(100) NOT NULL,         -- Tên sản phẩm
+    Category VARCHAR(50) NOT NULL,             -- Loại sản phẩm
+    Price DECIMAL(10, 2) NOT NULL,             -- Giá sản phẩm
+    Stock INT NOT NULL                         -- Số lượng tồn kho
+);
+-- Thêm dữ liệu vào bảng Products
+INSERT INTO Products (ProductName, Category, Price, Stock)
+VALUES
+    ('iPhone 14', 'Electronics', 1000.00, 50),
+    ('MacBook Air', 'Electronics', 1200.00, 30),
+    ('T-Shirt', 'Fashion', 20.00, 200),
+    ('Sneakers', 'Fashion', 100.00, 100),
+    ('Refrigerator', 'Appliances', 800.00, 10),
+    ('Air Conditioner', 'Appliances', 600.00, 15),
+    ('Laptop', 'Electronics', 1500.00, 25),
+    ('Headphones', 'Electronics', 200.00, 75),
+    ('Jacket', 'Fashion', 150.00, 50),
+    ('Washing Machine', 'Appliances', 700.00, 8);
+    
+    #2
+    select ProductName, Category, Price from Products where Price > (select Price from Products where ProductName = 'MacBook Air');
+    
+    ## Em chưa có thời gian nên em đang làm dở bài 5 này, khi nào có thời gian em sẽ bổ sung nốt các bài tập.
+    ## Mong thầy thông cảm
+    #3
+    
+    
+
+
