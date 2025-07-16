976756d3c85364ea8f9d84161e168b06920cc78f
haonguyen118 <mailatoi88@gmail.com>
2025-07-16 23:37:22 +0900

    update

--- Bai08.sql
+++ Bai08.sql
@@ -9,8 +9,15 @@ insert into OrderDetail (priceBuy, product_id, quantity)
 values (120.00, 1, 12),
 (150.00,2,15),
 (250.00,3,25);
+alter table OrderDetail add column order_id int not null references orders (id) ;
+
+
 #1.Viết truy vấn để lấy thông tin đơn hàng cùng với tên khách hàng và tổng giá trị đơn hàng.
-select  distinct o.id,o.customer_id,o.order_date, c.customername from orders o join customers c on o.customer_id = c.id;
+select  distinct o.*, c.customername, sum( od.priceBuy*od.quantity) as total_money
+	from orders o
+	join orderdetail od on o.id = od.order_id
+	join customers c on o.customer_id = c.id
+    group by od.order_id;
 
 #.Tìm tất cả sản phẩm có giá trên 100 và đã được đặt hàng.
 select p.id, p.productname, p.price,p.stock_quantity
